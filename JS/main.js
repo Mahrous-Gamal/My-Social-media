2fec33496a10a87633f73a33fea66a748b260a0a
mahrousgamal <mahrousgamal2001@gmail.com>
2023-04-20 12:45:27 -0700

    first

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+document.querySelector(document).ready(function () {
+    document.querySelector(".trigger").addEventListener("click", function () {
+        document.querySelector(".menu").classList.toggle("active");
+    });
+});
